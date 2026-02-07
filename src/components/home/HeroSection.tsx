dd2bd933c3c024969a4ac96100007878f97d9383
Pratham Goswami <goswamipratham21112001@gmail.com>
2026-02-07 12:30:43 +0530

    head change

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
             transition={{ duration: 0.8, delay: 0.2 }}
             className="text-5xl md:text-6xl lg:text-7xl font-gilroy font-bold leading-[0.85] mb-10 tracking-tighter"
           >
-            <span className="text-white">Decision grade water intelligence</span>
+            <span className="text-white">SOLAR</span>
             <br />
             <div className="text-gradient italic">Simplified. Decentralized.</div>
           </motion.h1>
